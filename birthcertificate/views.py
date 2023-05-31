dfe713af49b7e79e67751431cdf2e48a853ade6e
kooroshkz <kooroshkz@yahoo.com>
2023-06-01 00:36:52 +0330

    Calculations added to Result.html

--- birthcertificate/views.py
+++ birthcertificate/views.py
@@ -12,6 +12,7 @@ now_year = jdatetime.datetime.now().year
 now_month = jdatetime.datetime.now().month
 now_day = jdatetime.datetime.now().day
 
+
 def index(request):
     if request.method == 'POST':
         form = BirthdayForm(request.POST)
@@ -24,7 +25,18 @@ def index(request):
             age = round(total_age, 1)
 
 
-            return render(request, 'birthcertificate/result.html', {'age': age})
+            return render(request, 'birthcertificate/result.html', {
+                'Sun': 0,
+                "Mercury" : age*365.25/88,
+                "Venus" : age*365.25/225,
+                'Earth': age,
+                "Mars" : age*365.25/(365.25+321.73),
+                "Jupiter" : age*365.25/(11*365.25+313.839),
+                "Satturn" : age*365.25/(29*365.25+167),
+                "Uranus" : age*365.25/(84*365.25+4),
+                "Neptun" : age*365.25/(164*365.25+288),
+                "Pluto" : age*365.25/(247*365.25+248)
+                })
     else:
         form = BirthdayForm()
     return render(request, 'birthcertificate/index.html', {'form': form})
